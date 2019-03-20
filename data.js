d7cc29d353a7c40e014e597d99a1b3b67b1056a5
Lee Changseok <leechangseok@Lees-MacBook-Pro.local>
2019-03-20 04:44:29 -0400

    image name typo in data.js find and fixed

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ exteriorInfo: "The new Hyundai Palisade measures 196.1 inches long, 77.8 inches 
 intImg:"interior.jpg",
 titleTwo:"Interior",
 interiorInfo:"The new Hyundai Palisade and the Chevy Traverse have similar interior dimensions, with Chevy winning in some areas, and the Hyundai winning in others. In terms of headroom, the Palisade offers 40.7 inches for the first row, 40.1 inches for the second row, and 37.2 inches for the third row. The Traverse has 41.3 inches, 40 inches, and 38. 2 inches, respectively.",
-engImg:"engine.jpg",
+engImg:"Engine.jpg",
 titleThird:"Performance",
 performanceInfo:"The 2020 Hyundai Palisade has a 3.8-liter V6 engine paired to an eight-speed automatic transmission. The duo is rated at 291 horsepower at 6,000 RPM and 262 pound-feet of torque. By comparison, the 3.6L V6 LFY in the Traverse makes 310 horsepower at 6,000 RPM and 266 pound-feet of torque. It is mated to the new GM 9-speed automatic transmission.",
 
